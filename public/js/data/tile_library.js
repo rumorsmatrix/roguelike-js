346c66bc8125848319008ce18b969cca2e90e512
rumorsmatrix <threesides@gmail.com>
2019-02-26 13:31:55 +0000

    updated gitignore, fixed some indentation

--- public/js/data/tile_library.js
+++ public/js/data/tile_library.js
@@ -5,13 +5,11 @@ game.tile_library = {};
 game.tile_library['player']         = new Tile("@", '#fff', '', true);
 game.tile_library['rat']            = new Tile("r", '#fff', '', true);
 
-// flooring
-game.tile_library['floor']          = new Tile(".", '#343', '#232',true);
-
-// walls
-game.tile_library['wall']           = new Tile("#", '#676', '#121',false);
+// floor and walls, used as defaults during map creation
+game.tile_library['floor']          = new Tile(".", '#343', '#232', true);
+game.tile_library['wall']           = new Tile("#", '#676', '#121', false);
 
 // doors
-game.tile_library['door_closed']    = new Tile("+", '#bbb', '#121',false);
-game.tile_library['door_open']      = new Tile("-", '#bbb', '#121',true);
+game.tile_library['door_closed']    = new Tile("+", '#bbb', '#121', false);
+game.tile_library['door_open']      = new Tile("'", '#bbb', '#121', true);
 
